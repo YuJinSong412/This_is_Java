eaa0b5e13e02cdab396d9cf165e15a61a32bc1b6
yujin <yujin@naver.com>
2018-03-09 23:35:30 +0900

    first commit

---
+++ src/Exercise02/Ex01.java
@@ -0,0 +1,14 @@
+package Exercise02;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		int value = 10;
+		
+		int result = value +10;
+		
+		System.out.println(result);
+
+	}
+
+}
