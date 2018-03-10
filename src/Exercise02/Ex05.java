6355cbf1ed988524a46ffac2b6f5b5c160d9fc78
yujin <yujin@naver.com>
2018-03-10 23:04:48 +0900

    Ex05

---
+++ src/Exercise02/Ex05.java
@@ -0,0 +1,16 @@
+package Exercise02;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		long var1 =10;
+		long var2 =20L;
+		//long var3 =10000000000;
+		long var4 =10000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+	}
+
+}
