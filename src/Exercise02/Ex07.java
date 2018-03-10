99617ff218057e60f644aaef81a74c41215209be
yujin <yujin@naver.com>
2018-03-10 23:13:09 +0900

    Ex07

---
+++ src/Exercise02/Ex07.java
@@ -0,0 +1,12 @@
+package Exercise02;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		byte byteValue = 10;
+		int intValue = byteValue;
+		System.out.println(intValue);
+
+	}
+
+}
