f3a111f293d79dae6c99ed27b40b33c20fdafcb5
yujin <yujin@naver.com>
2018-03-10 23:22:18 +0900

    ex08

---
+++ src/Exercise02/Ex08.java
@@ -0,0 +1,18 @@
+package Exercise02;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		int i =128;
+		
+		if((i<Byte.MIN_VALUE)|| (i>Byte.MAX_VALUE)) {
+			System.out.println("byte 타입으로 변환할 수 없습니다.");
+			System.out.println("값을 다시 확인해 주세요.");
+		}else {
+			byte b = (byte)i;
+			System.out.println(b);
+		}
+
+	}
+
+}
