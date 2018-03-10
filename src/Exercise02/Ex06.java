a06cdc1cadb3ba17141ac4b6028e5906c3101241
yujin <yujin@naver.com>
2018-03-10 23:08:53 +0900

    Ex06

---
+++ src/Exercise02/Ex06.java
@@ -0,0 +1,17 @@
+package Exercise02;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		boolean stop = true;
+		
+		if(stop) {
+			System.out.println("중지합니다.");
+		}
+		else {
+			System.out.println("시작합니다.");
+		}
+
+	}
+
+}
