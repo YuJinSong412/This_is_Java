25586159ca4861819d4a386fed2f5c1f29e85c3a
yujin <yujin@naver.com>
2018-03-11 16:38:32 +0900

    Ex03

---
+++ src/Exercise04/Ex03.java
@@ -0,0 +1,15 @@
+package Exercise04;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		int sum =0;
+		
+		for(int i=1;i<=100;i++) {
+			sum +=i;
+		}
+		System.out.println("1~100합: "+sum);
+
+	}
+
+}
