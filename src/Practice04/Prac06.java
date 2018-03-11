5daa3099932394ce392044e5b58edceea26731de
yujin <yujin@naver.com>
2018-03-11 17:31:50 +0900

    Prac06

---
+++ src/Practice04/Prac06.java
@@ -0,0 +1,15 @@
+package Practice04;
+
+public class Prac06 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
