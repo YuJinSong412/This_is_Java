1a93f81863e1a10982a35bca79cf2bd55e2c9604
yujin <yujin@naver.com>
2018-03-11 16:48:23 +0900

    Ex06

---
+++ src/Exercise04/Ex06.java
@@ -0,0 +1,17 @@
+package Exercise04;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		int i=1;
+		int sum=0;
+		
+		while(i<=100) {
+			sum +=i;
+			i++;
+		}
+		System.out.println("1~"+(i-1)+"합: "+sum);
+
+	}
+
+}
