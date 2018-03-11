4fd94dc90c11bfb83c624a38c9baef53d0b251ef
yujin <yujin@naver.com>
2018-03-11 15:19:15 +0900

    Prac06

---
+++ src/Practice03/Prac06.java
@@ -0,0 +1,14 @@
+package Practice03;
+
+public class Prac06 {
+
+	public static void main(String[] args) {
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int height = 7;
+		double area = (lengthTop+lengthBottom)*7*0.5;
+		System.out.println(area);
+
+	}
+
+}
