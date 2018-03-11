4a1b74546bf6a0d8bc0531864cfe2a245dd47fc0
yujin <yujin@naver.com>
2018-03-11 16:38:40 +0900

    Ex04

---
+++ src/Exercise04/Ex04.java
@@ -0,0 +1,15 @@
+package Exercise04;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		for(int i=2;i<=9;i++) {
+			System.out.println("*** "+i+"단 ***");
+			for(int j=1;j<=9;j++) {
+				System.out.println(i+" x "+j+" = "+i*j);
+			}
+		}
+
+	}
+
+}
