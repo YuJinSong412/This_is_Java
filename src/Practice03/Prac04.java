0da56f0bed05b81bcb1d9269ec802baf6ef059a1
yujin <yujin@naver.com>
2018-03-11 15:18:57 +0900

    Prac04

---
+++ src/Practice03/Prac04.java
@@ -0,0 +1,17 @@
+package Practice03;
+
+public class Prac04 {
+
+	public static void main(String[] args) {
+		int pencils = 534;
+		int students = 30;
+		
+		int pencilsPerStudent = pencils/students;
+		System.out.println(pencilsPerStudent);
+		
+		int pencilsLeft = pencils % students;
+		System.out.println(pencilsLeft);
+
+	}
+
+}
