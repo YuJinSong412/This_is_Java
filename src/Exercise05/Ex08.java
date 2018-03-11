6b279f29e1ef4ecaf755f0eb73760cbfd8e9e94e
yujin <yujin@naver.com>
2018-03-11 23:06:08 +0900

    Ex08

---
+++ src/Exercise05/Ex08.java
@@ -0,0 +1,43 @@
+package Exercise05;
+import java.util.*;
+public class Ex08 {
+
+	public static void main(String[] args) {
+		Week today = null;
+		
+		Calendar cal = Calendar.getInstance();
+		int week = cal.get(Calendar.DAY_OF_WEEK);
+		
+		switch(week) {
+		case 1:
+			today = Week.SUNDAY;
+			break;
+		case 2:
+			today = Week.MONDAY;
+			break;
+		case 3:
+			today = Week.TUESDAY;
+			break;
+		case 4:
+			today = Week.WEDNESDAY;
+			break;
+		case 5:
+			today = Week.THURSDAY;
+			break;
+		case 6:
+			today = Week.FRIDAY;
+		case 7:
+			today = Week.SUNDAY;
+			break;
+		}
+		System.out.println("오늘 요일: "+today);
+		
+		if(today==Week.SUNDAY) {
+			System.out.println("일요일에는 축구를 합니다.");
+		}else {
+			System.out.println("열심히 자바 공부합니다.");
+		}
+
+	}
+
+}
