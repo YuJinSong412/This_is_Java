56069a86c4beb4c3608ec01e07903892e4ce46b1
yujin <yujin@naver.com>
2018-03-11 17:45:59 +0900

    Prac05

--- src/Practice04/Prac05.java
+++ src/Practice04/Prac05.java
@@ -4,7 +4,6 @@ public class Prac05 {
 
 	public static void main(String[] args) {
 		for(int i=1;i<=10;i++) {
-			
 			for(int j=1;j<=1;j++) {
 				if((60-(4*i))%5==0) {
 					System.out.println("("+i+","+(60-4*i)/5+")");
