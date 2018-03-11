dd5a135c52606db943534ee6d48fa90c51bb8865
yujin <yujin@naver.com>
2018-03-11 14:55:57 +0900

    Ex07

---
+++ src/Exercise03/Ex07.java
@@ -0,0 +1,18 @@
+package Exercise03;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		String strVar1 = "신민철";
+		String strVar2 = "신민철";
+		String strVar3 = new String("신민철");
+		
+		System.out.println(strVar1 == strVar2);
+		System.out.println(strVar1 == strVar3);
+		System.out.println();
+		System.out.println(strVar1.equals(strVar2));
+		System.out.println(strVar1.equals(strVar3));
+
+	}
+
+}
