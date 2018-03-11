dc12fdbd99abec650009643d0ba33012da7b3d6b
yujin <yujin@naver.com>
2018-03-11 18:49:39 +0900

    ex

---
+++ src/Exercise05/Ex01.java
@@ -0,0 +1,33 @@
+package Exercise05;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		String strVar1 = "신민철";
+		String strVar2 = "신민철";
+		
+		if(strVar1==strVar2) {
+			System.out.println("strVar1 과 strVar2는 참조가 같음");
+		}else {
+			System.out.println("strVar1 과 strVar2는 참조가 다름");
+		}
+		
+		if(strVar1.equals(strVar2)) {
+			System.out.println("strVar1 과 strVar2는 문자열이 같음");
+		}
+		
+		String strVar3 = new String("신민철");
+		String strVar4 = new String("신민철");
+		
+		if(strVar3==strVar4) {
+			System.out.println("strVar3 과 strVar4는 참조가 같음");
+		}else {
+			System.out.println("strVar3 과 strVar4는 참조가 다름");
+		}
+		
+		if(strVar3.equals(strVar4)) {
+			System.out.println("strVar3 과 strVar4는 문자열이 같음");
+		}
+	}
+
+}
