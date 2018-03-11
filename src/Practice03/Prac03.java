d65881e48e9baee4ef20266745c1485a1b98bee1
yujin <yujin@naver.com>
2018-03-11 15:18:48 +0900

    Prac03

---
+++ src/Practice03/Prac03.java
@@ -0,0 +1,12 @@
+package Practice03;
+
+public class Prac03 {
+
+	public static void main(String[] args) {
+		int score = 85;
+		
+		String result = (!(score>90)? "가":"나");
+		System.out.println(result);
+	}
+
+}
