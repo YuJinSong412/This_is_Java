72022a7c386c184143e6d4e738a3c1a43ce9ae85
yujin <yujin@naver.com>
2018-03-11 23:42:09 +0900

    Prac

---
+++ src/Practice05/Prac06.java
@@ -0,0 +1,16 @@
+package Practice05;
+
+public class Prac06 {
+
+	public static void main(String[] args) {
+		int[][] array = {
+				{95,86},
+				{83,92,96},
+				{78,83,93,87,88}
+		};
+		
+		System.out.println(array.length);
+		System.out.println(array[2].length);
+	}
+
+}
