614ff5ced2b2ba3bff76d5373b70e06e1a1ae8cc
yujin <yujin@naver.com>
2018-03-11 19:29:44 +0900

    Ex05

---
+++ src/Exercise05/Ex05.java
@@ -0,0 +1,19 @@
+package Exercise05;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		int[] oldIntArray = {1,2,3};
+		int[] newIntArray = new int[5];
+		
+		for(int i=0;i<oldIntArray.length;i++) {
+			newIntArray[i] = oldIntArray[i];
+		}
+		
+		for(int i=0;i<newIntArray.length;i++) {
+			System.out.print(newIntArray[i]+",");
+		}
+
+	}
+
+}
