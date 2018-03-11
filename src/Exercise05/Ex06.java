d7e523d1e0512aed7253b46c7aba87f74dc0ca6b
yujin <yujin@naver.com>
2018-03-11 19:32:48 +0900

    Ex06

---
+++ src/Exercise05/Ex06.java
@@ -0,0 +1,17 @@
+package Exercise05;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		String[] oldStrArray = {"java","array","copy"};
+		String[] newStrArray = new String[5];
+		
+		System.arraycopy(oldStrArray,0,newStrArray,0,oldStrArray.length);
+		
+		for(int i=0;i<newStrArray.length;i++) {
+			System.out.print(newStrArray[i]+",");
+		}
+
+	}
+
+}
