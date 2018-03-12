483f0ddffd6c0eec0d9e598018c501e2547fd40d
yujin <yujin@naver.com>
2018-03-12 18:49:33 +0900

    Ex11

---
+++ src/Exercise06/Ex11.java
@@ -0,0 +1,24 @@
+package Exercise06;
+
+class Person{
+	final String nation = "Korea";
+	final String ssn;
+	String name;
+	
+	Person(String ssn, String name){
+		this.ssn=ssn;
+		this.name = name;
+	}
+}
+public class Ex11 {
+
+	public static void main(String[] args) {
+		Person p1 = new Person("1234455","송유진");
+		
+		System.out.println(p1.nation);
+		System.out.println(p1.ssn);
+		System.out.println(p1.name);
+
+	}
+
+}
