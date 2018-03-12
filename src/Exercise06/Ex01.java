9b6a4c5b0aaa4b78a577e52523b6bfeb6228a570
yujin <yujin@naver.com>
2018-03-12 16:17:11 +0900

    ff

---
+++ src/Exercise06/Ex01.java
@@ -0,0 +1,17 @@
+package Exercise06;
+
+class Student{
+	
+}
+public class Ex01 {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println("s1 변수가 Student 객체를 참조합니다.");
+		
+		Student s2 = new Student();
+		System.out.println("s2 변수가 Student 객체를 참조합니다.");
+
+	}
+
+}
