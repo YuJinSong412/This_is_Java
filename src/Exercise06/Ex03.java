c3f4c0841d13d7ec619e33d24fcd45f22154e4c4
yujin <yujin@naver.com>
2018-03-12 17:02:56 +0900

    Ex03

---
+++ src/Exercise06/Ex03.java
@@ -0,0 +1,26 @@
+package Exercise06;
+
+class Korean{
+	String nation = "대한민국";
+	String name;
+	String ssn;
+	
+	 Korean(String n, String s) {
+		name = n;
+		ssn =s;
+	}
+}
+public class Ex03 {
+
+	public static void main(String[] args) {
+		Korean k1 = new Korean("박자바","01050271921");
+		System.out.println("k1.name: "+k1.name);
+		System.out.println("k1.ssn: "+k1.ssn);
+		
+		Korean k2 = new Korean("김자바","01050271921");
+		System.out.println("k2.name: "+k2.name);
+		System.out.println("k2.ssn: "+k2.ssn);
+
+	}
+
+}
