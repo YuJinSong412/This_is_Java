72a2944c5c70fc89ab025a3772d49e20f3ab51e7
yujin <yujin@naver.com>
2018-03-12 17:42:46 +0900

    Ex06

---
+++ src/Exercise06/Ex06.java
@@ -0,0 +1,38 @@
+package Exercise06;
+
+class Computer{
+	int sum1(int[] values) {
+		int sum=0;
+		for(int i=0;i<values.length;i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
+	int sum2(int ... values) {
+		int sum=0;
+		for(int i=0;i<values.length;i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
+}
+public class Ex06 {
+
+	public static void main(String[] args) {
+		Computer myCom = new Computer();
+		
+		int [] values1 = {1,2,3};
+		int result1 = myCom.sum1(values1);
+		System.out.println("result1: "+result1);
+		
+		int result2 = myCom.sum1(new int[] {1,2,3,4,5});
+		System.out.println("result2: "+result2);
+		
+		int result3 = myCom.sum2(1,2,3);
+		System.out.println("result3: "+result3);
+		
+		int result4 = myCom.sum2(1,2,3,4,5);
+		System.out.println("result4: "+result4);
+	}
+
+}
