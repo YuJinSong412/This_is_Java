949e2624d0781769ea59b62ea7d2c768b7e824bb
yujin <yujin@naver.com>
2018-03-12 11:48:07 +0900

    Prac08

---
+++ src/Practice05/Prac08.java
@@ -0,0 +1,27 @@
+package Practice05;
+
+public class Prac08 {
+
+	public static void main(String[] args) {
+		int [][] array = {
+				{95,86},
+				{83,92,96},
+				{78,83,93,87,88}
+		};
+		int sum = 0;
+		double avg = 0.0;
+		
+		for(int i=0;i<array.length;i++) {
+			for(int j=0;j<array[i].length;j++) {
+				sum += array[i][j];
+				
+			}
+		}
+		avg = (double)sum/10;
+		
+		System.out.println("sum: "+sum);
+		System.out.println("avg: "+avg);
+
+	}
+
+}
