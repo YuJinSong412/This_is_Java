266cc4b1201e24726d1740d8aa93d8842ad4c431
yujin <yujin@naver.com>
2018-03-14 18:22:46 +0900

    Ex03

---
+++ src/Exercise08/Ex03.java
@@ -0,0 +1,16 @@
+package Exercise08;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+/*
+		RemoteControl rc = new RemoteControl() {
+			public void turnOn() {}
+			public void turnOff() {}
+			public void setVolume(int volume) {}
+			
+		};
+*/
+	}
+
+}
