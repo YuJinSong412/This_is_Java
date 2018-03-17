f620a168c2dc61014575de965d6f44221e21351c
yujin <yujin@naver.com>
2018-03-17 09:50:40 +0900

    Ex01

--- src/Exercise09/Ex01.java
+++ src/Exercise09/Ex01.java
@@ -1,9 +1,58 @@
 package Exercise09;
 
+class A{
+	A(){
+		System.out.println("A 객체가 생성됨");
+	}
+	class B{
+		B(){
+			System.out.println("B 객체가 생성됨");
+		}
+		int field1;
+		//static int field2;
+		void method1() {}
+		//static method2(){}
+	}
+	
+	static class C{
+		C(){
+			System.out.println("C 객체가 생성됨");
+		}
+		int field1;
+		static int field2;
+		void method1() {}
+		static void method2() {}
+	}
+	
+	void method() {
+		class D{
+			D(){
+				System.out.println("D 객체가 생성됨");
+			}
+			int field1;
+			void method1() {}
+		}
+		D d = new D();
+		d.field1 = 3;
+		d.method1();
+	}
+}
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		A a = new A();
+		
+		A.B b = a.new B();
+		b.field1 =3;
+		b.method1();
+		
+		A.C c = new A.C();
+		c.field1 = 3;
+		c.method1();
+		A.C.field2 = 3;
+		A.C.method2();
+		
+		a.method();
 
 	}
 
