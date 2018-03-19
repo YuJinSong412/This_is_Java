e6fa6ba5f2f58824734c1fe3d1283295597b9e0d
yujin <yujin@naver.com>
2018-03-19 19:01:04 +0900

    Ex

---
+++ src/Exercise10/Ex01.java
@@ -0,0 +1,14 @@
+package Exercise10;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		try {
+			Class clazz = Class.forName("java.lang.String2");
+		}catch(ClassNotFoundException e) {
+			System.out.println("클래스가 존재하지 않습니다.");
+		}
+
+	}
+
+}
