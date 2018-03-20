051fd8c90a6282b27dde4092bc843ad18f0b3de5
yujin <yujin@naver.com>
2018-03-20 14:55:37 +0900

    Ex17

---
+++ src/Exercise11/Ex17.java
@@ -0,0 +1,11 @@
+package Exercise11;
+
+public class Ex17 {
+
+	public static void main(String[] args) {
+		String javaHome = System.getenv("JAVA_HOME");
+		System.out.println("JAVA_HONE: "+javaHome);
+
+	}
+
+}
