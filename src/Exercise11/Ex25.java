37207171a1f985a6f6aee7f67aa2c2e435423418
yujin <yujin@naver.com>
2018-03-20 15:55:14 +0900

    Ex25

---
+++ src/Exercise11/Ex25.java
@@ -0,0 +1,13 @@
+package Exercise11;
+
+public class Ex25 {
+
+	public static void main(String[] args) {
+		String oldstr = "자바는 객체지향언어 입니다. 자바는 풍부한 API를 지원합니다.";
+		String newstr = oldstr.replace("자바", "JAVA");
+		System.out.println(oldstr);
+		System.out.println(newstr);
+
+	}
+
+}
