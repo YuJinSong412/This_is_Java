4c12cea12ccfbab52a08f37473d65e7f48cdc51f
yujin <yujin@naver.com>
2018-03-20 11:25:21 +0900

    Ex11

---
+++ src/Exercise11/Ex01.java
@@ -0,0 +1,41 @@
+package Exercise11;
+
+class Member{
+	String id;
+	
+	Member(String id){
+		this.id = id;
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member member = (Member)obj;
+			if(id.equals(member.id)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+public class Ex01 {
+
+	public static void main(String[] args) {
+		Member obj1 = new Member("blue");
+		Member obj2 = new Member("blue");
+		Member obj3 = new Member("red");
+		
+		if(obj1.equals(obj2)) {
+			System.out.println("동등합니다.");
+		}else {
+			System.out.println("동등하지 않습니다.");
+		}
+		
+		if(obj1.equals(obj3)) {
+			System.out.println("동등합니다.");
+		}else {
+			System.out.println("동등하지 않습니다.");
+		}
+
+	}
+
+}
