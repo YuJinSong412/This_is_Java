532b9f9734e7c1e3aeafc0a3c0a578a62c6c6eed
yujin <yujin@naver.com>
2018-03-20 12:01:50 +0900

    Ex04

---
+++ src/Exercise11/Ex04.java
@@ -0,0 +1,16 @@
+package Exercise11;
+
+import java.util.Date;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		Object obj = new Object();
+		Date obj2 = new Date();
+		
+		System.out.println(obj.toString());
+		System.out.println(obj2.toString());
+
+	}
+
+}
