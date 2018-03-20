1d0ac5f943a94d4793d35c98aea74c0ed3aa7749
yujin <yujin@naver.com>
2018-03-20 16:07:07 +0900

    Ex26

---
+++ src/Exercise11/Ex26.java
@@ -0,0 +1,15 @@
+package Exercise11;
+
+public class Ex26 {
+
+	public static void main(String[] args) {
+		String str1 = String.valueOf(10);
+		String str2 = String.valueOf(10.5);
+		String str3 = String.valueOf(true);
+
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+
+}
