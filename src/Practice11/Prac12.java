5b7a1394afbe271bd8fa143deeeb4d81c6325aed
yujin <yujin@naver.com>
2018-03-22 19:23:04 +0900

    Prac

---
+++ src/Practice11/Prac12.java
@@ -0,0 +1,15 @@
+package Practice11;
+
+public class Prac12 {
+
+	public static void main(String[] args) {
+		String str = "200";
+		int intData = Integer.parseInt("200");
+		System.out.println(intData);
+		
+		int intData2 = 150;
+		String strData = Integer.toString(intData2);
+		System.out.println(strData);
+	}
+
+}
