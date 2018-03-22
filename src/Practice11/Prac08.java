833d0bda51ae286d39b45967b62ab808ce6741bd
yujin <yujin@naver.com>
2018-03-22 19:22:52 +0900

    Prac08

---
+++ src/Practice11/Prac08.java
@@ -0,0 +1,16 @@
+package Practice11;
+
+public class Prac08 {
+
+	public static void main(String[] args) {
+		String str = "";
+		for(int i=1;i<=100;i++) {
+			str +=i;
+		}
+		System.out.println(str);
+		
+		StringBuilder sb = new StringBuilder();
+
+	}
+
+}
