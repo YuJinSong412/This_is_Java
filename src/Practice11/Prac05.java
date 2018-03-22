ff1c0fd555fb58db03538180eb1d3a90d055c904
yujin <yujin@naver.com>
2018-03-22 19:03:10 +0900

    Prac:

---
+++ src/Practice11/Prac05.java
@@ -0,0 +1,12 @@
+package Practice11;
+
+public class Prac05 {
+
+	public static void main(String[] args) {
+		byte [] bytes = {73,32,55,24,23,23,22,11,22,123,24};
+		
+		String str = new String(bytes);
+		System.out.println(str);
+	}
+
+}
