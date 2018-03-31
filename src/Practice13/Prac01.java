6ac4522b83084ff6aac41c42abab135ba7d26317
yujin <yujin@naver.com>
2018-03-31 12:31:46 +0900

    Prac

---
+++ src/Practice13/Prac01.java
@@ -0,0 +1,26 @@
+package Practice13;
+
+class Container<T>{
+	private T t;
+	public void set(T t) {
+		this.t = t;
+	}
+	public T get() {
+		return t;
+	}
+	
+}
+public class Prac01 {
+
+	public static void main(String[] args) {
+		Container<String> container1 = new Container<String>();
+		container1.set("홍길동");
+		String str = container1.get();
+		
+		Container<Integer> container2 = new Container<Integer>();
+		container2.set(6);
+		int value = container2.get();
+
+	}
+
+}
